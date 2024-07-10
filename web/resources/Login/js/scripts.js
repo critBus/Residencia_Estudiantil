1b758bd20b37af4d8ffa7e2e0ef1403ea45c6783
René Lázaro Collado Arteaga <94305086+critBus@users.noreply.github.com>
2024-07-10 14:16:55 -0400

    refactor: recursos

---
+++ web/resources/Login/js/scripts.js
@@ -0,0 +1,33 @@
+
+jQuery(document).ready(function() {
+
+    /*
+        Background slideshow
+    */
+    $.backstretch([
+      "resources/Login/img/backgrounds/0.jpg"
+    , "resources/Login/img/backgrounds/1.jpg"
+    , "resources/Login/img/backgrounds/2.jpg"
+    , "resources/Login/img/backgrounds/3.jpg"
+    , "resources/Login/img/backgrounds/4.jpg"
+    , "resources/Login/img/backgrounds/5.jpg"
+    , "resources/Login/img/backgrounds/6.jpg"
+    , "resources/Login/img/backgrounds/7.jpg"
+    , "resources/Login/img/backgrounds/8.jpg"
+    , "resources/Login/img/backgrounds/9.jpg"
+    , "resources/Login/img/backgrounds/10.jpg"
+    , "resources/Login/img/backgrounds/11.jpg"
+    , "resources/Login/img/backgrounds/12.jpg"
+    , "resources/Login/img/backgrounds/13.jpg"
+    , "resources/Login/img/backgrounds/14.jpg"
+    , "resources/Login/img/backgrounds/15.jpg"
+    , "resources/Login/img/backgrounds/16.jpg"
+    , "resources/Login/img/backgrounds/17.jpg"
+    , "resources/Login/img/backgrounds/18.jpg"
+    , "resources/Login/img/backgrounds/19.jpg"
+    , "resources/Login/img/backgrounds/20.jpg"
+    ], {duration: 3000, fade: 1200});
+
+});
+
+
